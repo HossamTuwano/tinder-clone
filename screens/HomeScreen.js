44583926dc3fe5b6179e26bb9ca8aef254f98d23
Hossam Tuwano <hossamtuwano@gmail.com>
2023-09-16 20:37:38 +0300

    added context provider

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,11 +3,16 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
-    const navigation = useNavigation();
-    return (
+  const navigation = useNavigation();
+  return (
     <View>
       <Text>I am the HomeScreen</Text>
-      <Button title="Go to Chat Screen" onPress={() => {navigation.navigate('Chat')}}/>
+      <Button
+        title="Go to Chat Screen"
+        onPress={() => {
+          navigation.navigate("Chat");
+        }}
+      />
     </View>
   );
 };
