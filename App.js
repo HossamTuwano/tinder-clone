192257a99ef6713d942114b1b30740f0fb1e9856
Hossam Tuwano <hossamtuwano@gmail.com>
2023-09-16 20:13:49 +0300

    added navigation

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import tw from "tailwind-rn";
+import { StyleSheet } from "react-native";
+import StackNavigator from "./StackNavigator";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={tw("flex-1 justify-end items-center")}>
-      <Text>Open up App.js to start working on your hooray!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
